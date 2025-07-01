3f1e823cbb866fb7003a783cab54786387eb09af
Jay Potter <j@ypotter.ca>
2025-07-01 17:58:35 -0400

    spacing

--- src/Potter/Resettable/ResettableInterface.php
+++ src/Potter/Resettable/ResettableInterface.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Potter\Resettable;
 
-interface ResettableInterface 
+interface ResettableInterface
+    extends \Monolog\ResettableInterface
 {
     public function reset(): void;
 }
